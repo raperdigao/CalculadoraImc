1a62569136bed5abda437dd3b9616d9cf1b667ad
Raphael Pagni Perdigão <110313895+raperdigao@users.noreply.github.com>
2024-02-24 15:36:34 -0300

    Finalização do site em html, css e javascript
    Calculadora de IMC.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,77 +1,151 @@
 //IMC
-//1. capturar os valores
-// Função para receber os valores dos inputs e converter o mesmo para um objeto
+//1. capturar os valores - ok
+//2. calcular o IMC -ok
+//3. Gerar a classificação do IMC - ok
+//4. Organizar as informações - ok
+//5. salvar os dados em uma lista (array) -> localStorage - ok
+//6. ler a lista com os dados(array) -> localStorage -> ok
+//7. renderizar o conteúdo na tela -> ok
+//8. botão limpar registros -> clear localStorage 
 
+//função responsável por chamar todas as outras e exibir o conteúdo na tela
+
+
+function calcularImc(event) {
+    event.preventDefault();
+
+    let dadosUsuario = pegarValores()
+
+    let imc = calcular( dadosUsuario.alturaUsuario, dadosUsuario.pesoUsuario )
+
+    let classificarIMC = classificacaoImc(imc)
+
+    let usuarioAtt = organizarDados(dadosUsuario, imc, classificarIMC)
+
+    cadastrarUsuario(usuarioAtt)
+
+    window.location.reload()
+
+    
+
+     // Rolar a página para baixo para a seção de resultados
+
+    //  Desce até a posição 0 do body
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+//Função para receber os valores dos inputs e converter o mesmo para um objeto
 function pegarValores() {
-    // trim é para remover os espações em branco
     let nome = document.getElementById('nome').value.trim();
-    let altura = parseFloat(document.getElementById('altura').ariaValueMax);
-    let peso = parseFloat(document.getElementById('peso').value);
+    let altura = parseFloat(document.getElementById('altura').value)
+    let peso = parseFloat(document.getElementById('peso').value)
 
-    // Transformou em objeto recebendo os dados do front end
-    let dadosUsuarios = {
+    //variável de objeto recebendo os dados do front-end
+    let dadosUsuario = {
         nomeUsuario: nome,
         alturaUsuario: altura,
         pesoUsuario: peso
     }
-    return dadosUsuarios;
+
+    return dadosUsuario;
 }
-//2. Gerar a classificação do IMC
+
 function calcular(altura, peso) {
     let imc = peso / (altura * altura)
+
     return imc;
 }
 
-//3. Gerar classificação do IMC
-
 function classificacaoImc(imc) {
     if (imc < 18) {
-        return "abaixo do peso"
+        return "abaixo do peso";
     } else if (imc < 25) {
         return "peso normal"
     } else if (imc < 30) {
         return "sobrepeso"
     } else if (imc < 35) {
-        return "obesidade 1"
+        return "Obesidade 1"
     } else {
         return "obesidade 2 e 3"
     }
 }
-//4. Organizar as informações com a data atual
-function organizarDados(dadosUsuarios, valorIMC, classificacaoImc) {
-    // Capturar a data e horario do brasil
-    let dataAtual = Intl.DateTimeFormat('pt-BR', {timeStyle: 'long', dateStyle: 'short'}).format(Date.now());
+
+function organizarDados(dadosUsuario, valorImc, classificacaoImc) {
+    let dataAtual = Intl.DateTimeFormat('pt-BR', { timeStyle: 'long', dateStyle: 'short' }).format(Date.now());
     let dadosUsuarioAtt = {
-        // Os tres pontos pega os dados do objeto dadosUsuarios
+
+         // Os tres pontos pega os dados do objeto dadosUsuarios
         // Operador Rest
-        ...dadosUsuarios, 
-        imc: valorIMC.toFixed(2),
+        ...dadosUsuario,
+        imc: valorImc.toFixed(2),
         classificacao: classificacaoImc,
         dataCadastro: dataAtual
     }
+
     return dadosUsuarioAtt;
-  }
+}
 
-
-//5. Salvar os dados em uma lista (ARRAY) -> lOCALSTORAGE
-// Função responsavel para armazenar objeto dentro do local storage
+//função responsável por armazenar o objeto dentro do local storage
 function cadastrarUsuario(usuario) {
     let listaDeUsuario = []
 
-    // Validação para verificar se eu tenho algum dado dentro do loval storage do navegador
-    // Se for verdadeiro
+    //validação para verificar se eu tenho algum dado dentro do local storage do navegador
     if (localStorage.getItem("usuariosCadastrados")) {
-        listaDeUsuario = JSON.parse(localStorage.getItem
-            ("usuariosCadastrados"))
-
+        listaDeUsuario = JSON.parse(localStorage.getItem("usuariosCadastrados"))
     }
-    // Se for falso cria um novo chave e valor
+
     listaDeUsuario.push(usuario)
-    localStorage.setItem("usuariosCadastrados", JSON.stringify
-    (listaDeUsuario))
+
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaDeUsuario))
 }
 
-//6. ler a lista com os dados (ARRAY) -> LOCALSTORAGE
-//7. renderizar o conteúdo na tela
-//8. botão limpar registro -> clear localstorage
+function carregarUsuarios() {
+    let listaUsuarios = []
+
+    if (localStorage.getItem("usuariosCadastrados")) {
+        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
+
+    if (listaUsuarios.length == 0) {
+        let tabela = document.getElementById('corpo-tabela')
+
+        tabela.innerHTML = `
+            <tr class="linha-mensagem">
+               <td colspan="6"> Nenhum usuário cadastrado </td> 
+            </tr>
+        `
+    } else {
+        montarTabela(listaUsuarios)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => carregarUsuarios());
+
+function montarTabela(listaDeCadastrados) {
+    let tabela = document.getElementById('corpo-tabela')
+
+    listaDeCadastrados.forEach(pessoa => {
+
+        tabela.innerHTML += `
+            <tr>
+                <td> ${pessoa.nomeUsuario} </td>
+                <td> ${pessoa.alturaUsuario} </td>
+                <td> ${pessoa.pesoUsuario} </td>
+                <td> ${pessoa.imc} </td>
+                <td> ${pessoa.classificacao} </td>
+                <td> ${pessoa.dataCadastro} </td>
+            </tr>
+        `
+    })
+}
+
+
+function deletarRegistros() {
+    localStorage.clear("usuariosCadastrados")
+
+    window.location.reload();
+ 
+
+}
+
 
